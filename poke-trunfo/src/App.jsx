16739ae603eb3765bd465f84337b3a7ebe6feb9a
chapetta <cha.petta@hotmail.com>
2025-09-10 17:03:38 -0300

    feat: implementacao do header, criaçao da pasta e arquivo css e implementação no app

--- poke-trunfo/src/App.jsx
+++ poke-trunfo/src/App.jsx
@@ -1,11 +1,13 @@
-
-import './App.css'
+import Header from './components/Header'
+import './styles/global.css'
 
 function App() {
   
 
   return (
-    <div></div>
+    <>
+    <Header />
+    </>
   )
 }
 
---
+++ poke-trunfo/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header class="header-container">
+      <h1 className="poketrunfo-Title">PokeTrunfo</h1>
+    </header>
+  );
+}
+
